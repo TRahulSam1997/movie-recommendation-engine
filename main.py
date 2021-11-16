7c56354babc31c796fbe9d258a49002d279c1182
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-11-16 21:43:27 +1100

    netflix csv added

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+import wget as wget
+# wget 'https://datasets.towardsai.net/combined_data_4.txt'
+# wget 'https://raw.githubusercontent.com/towardsai/tutorials/master/recommendation_system_tutorial/movie_titles.csv'
+# wget 'https://raw.githubusercontent.com/towardsai/tutorials/master/recommendation_system_tutorial/new_features.csv'
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from datetime import datetime
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import os
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+from scipy import sparse
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics import mean_squared_error
+
+# import xgboost as xgb
+from surprise import Reader, Dataset
+from surprise import BaselineOnly
+from surprise import KNNBaseline
+from surprise import SVD
+from surprise import SVDpp
+from surprise.model_selection import GridSearchCV
+
+def load_data():
+    netflix_csv_file = open("netflix_rating.csv", mode = "w")
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
