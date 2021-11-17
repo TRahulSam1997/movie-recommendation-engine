30acf9f7d23fe53682a72659bf15cd437d75ae85
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-11-17 23:30:10 +1100

    training data updates

--- main.py
+++ main.py
@@ -23,13 +23,14 @@ from surprise import SVD
 from surprise import SVDpp
 from surprise.model_selection import GridSearchCV
 
+
 def load_data():
     netflix_csv_file = open("netflix_rating.csv", mode = "w")
     rating_files = ['combined_data_4.txt']
     for file in rating_files:
         with open(file) as f:
             for line in f:
-                line.strip()
+                line = line.strip()
                 if line.endswith(":"):
                     movie_id = line.replace(":", "")
                 else:
@@ -37,27 +38,46 @@ def load_data():
                     row_data = [item for item in line.split(",")]
                     row_data.insert(0, movie_id)
                     netflix_csv_file.write(",".join(row_data))
-                    netflix_csv_file.write("\n")
+                    netflix_csv_file.write('\n')
 
     netflix_csv_file.close()
     df = pd.read_csv('netflix_rating.csv', sep=",", names = ["movie_id","customer_id", "rating", "date"])
     return df
 
-# netflix_rating_df = load_data()
-# netflix_rating_df
-# netflix_rating_df.head()
-#
-# netflix_rating_df.duplicated(["movie_id","customer_id", "rating", "date"]).sum()
-#
-# split_value = int(len(netflix_rating_df) * 0.80)
-# train_data = netflix_rating_df[:split_value]
-# test_data = netflix_rating_df[split_value:]
 
+netflix_rating_df = load_data()
+netflix_rating_df
+netflix_rating_df.head()
 
+netflix_rating_df.duplicated(["movie_id","customer_id", "rating", "date"]).sum()
+
+split_value = int(len(netflix_rating_df) * 0.80)
+train_data = netflix_rating_df[:split_value]
+test_data = netflix_rating_df[split_value:]
+
+plt.figure(figsize=(12, 8))
+ax = sns.countplot(x="rating", data=train_data)
+
+ax.set_yticklabels([num for num in ax.get_yticks()])
+
+plt.tick_params(labelsize=15)
+plt.title("Count Ratings in train data", fontsize=20)
+plt.xlabel("Ratings", fontsize=20)
+plt.ylabel("Number of Ratings", fontsize=20)
+plt.show()
+
+def get_user_item_sparse_matrix(df):
+    sparse_data = sparse.csr_matrix((df.rating, (df.customer_id, df.movie_id)))
+    return sparse_data
+
+train_sparse_data = get_user_item_sparse_matrix(train_data)
+
+test_sparse_data = get_user_item_sparse_matrix(test_data)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
